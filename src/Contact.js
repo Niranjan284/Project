8b41c3f2a6023c844a4014fc2e8ebe2a5bccc5b8
Niranjan <150768508+Niranjan284@users.noreply.github.com>
2024-11-13 22:12:14 +0530

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>If you have any questions, feel free to reach out to us at support@giftportal.com.</p>
+      <br></br>
+      <h1>Help us...</h1>
+    </div>
+  );
+};
+
+export default Contact;
